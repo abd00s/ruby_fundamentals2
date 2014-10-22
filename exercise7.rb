dfad29e173ccd6afd77da74b8204b2775eb05234
Abdullah <abd00s@users.noreply.github.com>
2014-10-22 15:26:59 -0400

    step three done using a method

--- exercise7.rb
+++ exercise7.rb
@@ -11,4 +11,10 @@ def how_many(people)
 	people.each {|cohort, students| puts "#{cohort}: #{students} students"}
 end
 
-how_many(students)
+students[:cohort4] = 43
+
+def cohort_names(people)
+	people.keys.each {|k| puts "The name of this cohort is: #{k}" }
+end
+
+cohort_names(students)
