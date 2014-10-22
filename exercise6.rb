6654920964d540d5d3001d9cd4953f775028233c
Abdullah <abd00s@users.noreply.github.com>
2014-10-22 15:06:12 -0400

    cleaned up and added notation to exercise 6

--- exercise6.rb
+++ exercise6.rb
@@ -9,16 +9,21 @@ end
 
 grocery_list << "rice"
 
+puts "Initially, our basket looks like this: " 
+puts
 display_items(grocery_list)
-
-puts grocery_list.length
-
+puts
+puts "The length of our array (number of items in our basket) is: #{grocery_list.length}" 
+puts
 puts grocery_list.include?("bananas")? "You need to pick up bananas" :  "You don't need to pick up bananas today"
-
+puts
 puts "The second item in the list is #{grocery_list[1]}"
-
+puts
+puts "Our items in alphabetical order are: "
+puts
 display_items(grocery_list.sort)
-
+puts
 grocery_list.delete("salmon")
-
+puts "Our modified items after remvoving salmon are: "
+puts
 display_items(grocery_list.sort)
