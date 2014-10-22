d7cacf0cf52e72a45a54ce5e5f26a641e8c97f3b
Abdullah <abd00s@users.noreply.github.com>
2014-10-22 15:56:44 -0400

    cleaned up the output of steps 1 thru 5

--- exercise7.rb
+++ exercise7.rb
@@ -11,13 +11,27 @@ def how_many(people)
 	people.each {|cohort, students| puts "#{cohort}: #{students.to_i} students"}
 end
 
+puts "To start with, our cohorts look as follows: "
+puts 
+how_many(students)
+
 students[:cohort4] = 43
 
 def cohort_names(people)
 	people.keys.each {|k| puts "The name of this cohort is: #{k}" }
 end
+puts
+puts "After we add our Fourth Cohort, the names (keys) of our cohorts are: "
+puts 
+cohort_names(students)
+puts
 
 students.each {|key, val| (students[key] *= 1.05)}
-
+puts "After expanding the capacity of the classes, our cohorts look like this: "
+puts 
+how_many(students)
+puts
 students.delete(:cohort2)
+puts "After deleting the second cohort, everything looks like: "
+puts
 how_many(students)
